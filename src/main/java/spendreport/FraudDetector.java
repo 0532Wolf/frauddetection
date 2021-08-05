d79d782f5d4fd4b9c3144c1ebafc0254aeee8e0a
0532Wolf <gouxiangxun@Gmail.com>
2021-08-05 14:42:42 +0800

    feat(数据逻辑添加): 添加两笔交易时间间隔的处理
    当标记状态被设置为 true 时，设置一个在当前时间一分钟后触发的定时器。
    当定时器被触发时，重置标记状态。
    当标记状态被重置时，删除定时器。

--- src/main/java/spendreport/FraudDetector.java
+++ src/main/java/spendreport/FraudDetector.java
@@ -39,11 +39,21 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 	private static final long serialVersionUID = 1L;
 	//判断上笔交易是否为小额交易状态
 	private transient ValueState<Boolean> flagState;
+	//时间间隔
+	private transient ValueState<Long> timerState;
 
 	private static final double SMALL_AMOUNT = 1.00;
 	private static final double LARGE_AMOUNT = 500.00;
 	private static final long ONE_MINUTE = 60 * 1000;
 
+	/**
+	 * 逻辑处理方法
+	 *  对于一个账户，如果出现小于 $1 美元的交易后紧跟着一个大于 $500 的交易，就输出一个报警信息。
+	 * @param transaction
+	 * @param context
+	 * @param collector
+	 * @throws Exception
+	 */
 	@Override
 	public void processElement(
 			Transaction transaction,
@@ -65,7 +75,15 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 			// Clean up our state
 			flagState.clear();
 		}
+		if (transaction.getAmount() < SMALL_AMOUNT) {
+			// set the flag to true
+			flagState.update(true);
 
+			// set the timer and timer state
+			long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+			context.timerService().registerProcessingTimeTimer(timer);
+			timerState.update(timer);
+		}
 		if (transaction.getAmount() < SMALL_AMOUNT) {
 			// Set the flag to true
 			flagState.update(true);
@@ -76,11 +94,47 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 		collector.collect(alert);*/
 	}
 
+	/**
+	 *
+	 * 当标记状态被设置为 true 时，设置一个在当前时间一分钟后触发的定时器。
+	 * 当定时器被触发时，重置标记状态。
+	 * 当标记状态被重置时，删除定时器。
+	 * @param parameters
+	 */
 	@Override
 	public void open(Configuration parameters) {
 		ValueStateDescriptor<Boolean> flagDescriptor = new ValueStateDescriptor<>(
 				"flag",
 				Types.BOOLEAN);
 		flagState = getRuntimeContext().getState(flagDescriptor);
+
+		ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<>(
+				"timer-state",
+				Types.LONG);
+		timerState = getRuntimeContext().getState(timerDescriptor);
+
+	}
+
+	/**
+	 * 当定时器触发时，将会调用 KeyedProcessFunction#onTimer 方法。 通过重写这个方法来实现一个你自己的重置状态的回调逻辑。
+	 * @param timestamp 时间
+	 * @param ctx
+	 * @param out
+	 */
+	@Override
+	public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) {
+		// remove flag after 1 minute
+		timerState.clear();
+		flagState.clear();
+	}
+
+	private void cleanUp(Context ctx) throws Exception {
+		// delete timer
+		Long timer = timerState.value();
+		ctx.timerService().deleteProcessingTimeTimer(timer);
+
+		// clean up all state
+		timerState.clear();
+		flagState.clear();
 	}
 }
